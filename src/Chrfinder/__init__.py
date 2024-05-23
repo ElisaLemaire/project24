a7cfcb7e53a7b6c588bc4b810abd00f13b457fab
dsanto03 <diogo.santosmartins@epfl.ch>
2024-05-23 21:35:46 +0200

    path changes

--- src/Chrfinder/__init__.py
+++ src/Chrfinder/__init__.py
@@ -4,9 +4,9 @@
 
 __version__ = "0.0.1"
 
-from .pubchemprops import get_cid_by_name, get_first_layer_props, get_second_layer_props
-from .pka_lookup import pka_lookup_pubchem
-from .your_main_module import add_molecule, find_pka, find_boiling_point, get_df_properties, det_chromato, update_results, main
+from Chrfinder.pubchemprops import get_cid_by_name, get_first_layer_props, get_second_layer_props
+from Chrfinder.pka_lookup import pka_lookup_pubchem
+from Chrfinder.Chrfinder import add_molecule, find_pka, find_boiling_point, get_df_properties, det_chromato, update_results, main
 
 __all__ = [
     'get_cid_by_name',
